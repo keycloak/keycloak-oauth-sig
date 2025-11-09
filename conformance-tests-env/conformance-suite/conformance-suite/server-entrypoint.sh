9c8d538c1de4b6ae8254e8bc635573c8b0f9706b
Thomas Darimont <thomas.darimont@googlemail.com>
2025-11-09 18:08:37 +0100

    Use conformance test branch with reduced log file noise
    - Fix order of system properties in java start command (-D must be used before -jar)
    - Silence warnings for invalid CSS generated by htmlunit
    - Disable printing full env to the log files
    
    Signed-off-by: Thomas Darimont <thomas.darimont@googlemail.com>

--- conformance-tests-env/conformance-suite/conformance-suite/server-entrypoint.sh
+++ conformance-tests-env/conformance-suite/conformance-suite/server-entrypoint.sh
@@ -3,9 +3,13 @@ cd conformance-suite
 mv -f run-tests.sh ./.gitlab-ci
 mvn package -DskipTests
 java -Xdebug -Xrunjdwp:transport=dt_socket,address=*:9999,server=y,suspend=n \
-    -jar target/fapi-test-suite.jar \
     -Djava.security.egd=file:/dev/./urandom \
+    -Dnet.openid.conformance.testModules.logFinalEnv=false \
+    -jar target/fapi-test-suite.jar \
     --fintechlabs.base_url=${CONFORMANCE_SERVER} \
     --fintechlabs.devmode=true \
     --fintechlabs.startredir=true \
-    --logging.level.net.openid.conformance.frontChannel=DEBUG
+    --logging.level.net.openid.conformance.frontChannel=DEBUG \
+    --logging.level.com.gargoylesoftware.htmlunit=ERROR \
+    --logging.level.org.htmlunit.DefaultCssErrorHandler=ERROR \
+    --logging.level.org.htmlunit.css.CssStyleSheet=ERROR
