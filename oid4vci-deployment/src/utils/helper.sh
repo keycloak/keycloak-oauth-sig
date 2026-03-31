ba852ffd893e0cdf901faf7c26ffa821b5c687e8
Ingrid Kamga <Ingrid.Kamga@adorsys.com>
2026-03-31 10:04:30 +0100

    Update credential offer retrieval for next release
    Signed-off-by: Ingrid Kamga <Ingrid.Kamga@adorsys.com>

--- oid4vci-deployment/src/utils/helper.sh
+++ oid4vci-deployment/src/utils/helper.sh
@@ -162,6 +162,11 @@ export_yaml_as_env() {
     [[ -n "${ISSUER_ENDPOINTS_FRONTEND:-}" ]] && export ISSUER_FRONTEND_URL="${ISSUER_ENDPOINTS_FRONTEND}"
     [[ -n "${CLIENTS_TEST_CLIENT:-}" ]] && export TEST_CLIENT_URL="${CLIENTS_TEST_CLIENT}"
     
+    # Collect features to start Keycloak with
+    KEYCLOAK_FEATURES="${KEYCLOAK_FEATURES:-oid4vc-vci}"
+    [[ "${KEYCLOAK_ENABLE_PREAUTH_CODE:-}" == "true" ]] && KEYCLOAK_FEATURES="$KEYCLOAK_FEATURES,oid4vc-vci-preauth-code"
+    export KEYCLOAK_FEATURES
+
     # Adjust KEYSTORE_PATH based on where Keycloak is running
     if [[ -n "${KEYSTORE_PATH:-}" ]]; then
         local docker_compose_cmd
