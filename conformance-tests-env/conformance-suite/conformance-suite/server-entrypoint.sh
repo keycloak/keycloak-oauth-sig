b04a44cc96e022285007e357208f95e20cd68c57
Takashi Norimatsu <takashi.norimatsu.ws@hitachi.com>
2026-04-13 15:23:09 +0900

    [GHA] run fapi1-advanced-final test for CI
    closes #961
    
    Signed-off-by: Takashi Norimatsu <takashi.norimatsu.ws@hitachi.com>

--- conformance-tests-env/conformance-suite/conformance-suite/server-entrypoint.sh
+++ conformance-tests-env/conformance-suite/conformance-suite/server-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 cd conformance-suite
-mv -f run-tests.sh ./.gitlab-ci
-mvn package -DskipTests
 java -Xdebug -Xrunjdwp:transport=dt_socket,address=*:9999,server=y,suspend=n \
     -jar target/fapi-test-suite.jar \
     -Djava.security.egd=file:/dev/./urandom \
