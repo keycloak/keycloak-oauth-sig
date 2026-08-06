56b5fdd8a51e2b960065e1ea861b3c7cc037105d
Vitalisn4 <ngamvitalisyuh@gmail.com>
2026-08-06 15:25:36 +0100

    chore: keep custom image entrypoint on oid4vc-vci only Revert KEYCLOAK_FEATURES wiring from docker-entrypoint so this PR stays scoped to the keystore path fix.
    Signed-off-by: Vitalisn4 <ngamvitalisyuh@gmail.com>

--- oid4vci-deployment/docker-entrypoint.sh
+++ oid4vci-deployment/docker-entrypoint.sh
@@ -25,5 +25,4 @@ export KC_BOOTSTRAP_ADMIN_PASSWORD="${KEYCLOAK_BOOTSTRAP_ADMIN_PASSWORD}"
 
 cd "$KEYCLOAK_INSTALL_DIR"
 
-# Features come from config.yaml (KEYCLOAK_FEATURES), including 26.7+ flags when enabled.
-eval "exec bin/kc.sh $START_COMMAND ${DATABASE_OPTS:-} --features=${KEYCLOAK_FEATURES}"
+eval "exec bin/kc.sh $START_COMMAND $DATABASE_OPTS --features=oid4vc-vci"
